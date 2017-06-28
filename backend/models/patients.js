a01dd200ed735bffb0b937fa5aa64c151ae482b3
Samitha Heanthenna <samitha071@gmail.com>
2017-06-29 00:35:09 +0530

    updated patient.js model

---
+++ backend/models/patients.js
@@ -0,0 +1,54 @@
+/**
+ * Created by SamithaHeanthenna-PC on 6/29/2017.
+ */
+
+var mongoose=require('mongoose');
+
+//this contains prescriptions model
+var patientsSchema = mongoose.Schema({
+    patientnic:{
+        type:String,
+        required:true
+    },
+    name:{
+        type:String,
+        required:true
+    },
+    age:{
+        type:String,
+        required:true
+    },
+    bloodgroup:{
+        type:String,
+        required:true
+    },
+    gender:{
+        type:String,
+        required:true
+    },
+
+    contactnumber:{
+        type:Number,
+        required:true
+    },
+
+    email:{
+        type:String,
+    },
+    patientdate:{
+        type:Date,
+        default:Date.now
+    }
+});
+
+var Patient = module.exports = mongoose.model('Patient',patientsSchema);
+
+//get all patients
+module.exports.getPatients = function (callback,limit) {
+    Patient.find(callback).limit(limit);
+}
+
+//get Patients by id
+module.exports.getPatientByID = function (id,callback) {
+    Patient.findById(id, callback);
+}
