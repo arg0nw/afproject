36f0c12f8655d9f7211b3b7cfa5613583a8702cb
dan0131 <danika.sack@yahoo.com>
2017-06-28 23:59:52 +0530

    editted drugs_home.html

--- client/controllers/drugs.js
+++ client/controllers/drugs.js
@@ -7,5 +7,16 @@ var myApp= angular.module('myApp');
 myApp.controller('DrugsController',['$scope', '$http', '$location', '$routeParams',function($scope, $http, $location, $routeParams) {
 	console.log('DrugsController');
 
-	
+	$scope.getDrugs = function(){
+		$http.get('/api/drugs').then(successCallback, errorCallback);
+		function successCallback(response){
+   			$scope.drugs=response.data;
+   			console.log($scope.drugs);
+   		
+		}
+		function errorCallback(error){
+   		
+   		
+		}
+	}
 }]);
