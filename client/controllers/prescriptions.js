d75e1d9ab7db0930eeea01ff6a24bf3276463f4f
Samitha Heanthenna <samitha071@gmail.com>
2017-06-29 05:50:39 +0530

    updated prescription controller

---
+++ client/controllers/prescriptions.js
@@ -0,0 +1,21 @@
+/**
+ * Created by SamithaHeanthenna-PC on 6/29/2017.
+ */
+
+//controller used for prescription controlling
+var myApp=angular.module('myApp');
+
+myApp.controller('PrescriptionsController',['$scope', '$http','$location','$routeParams', function ($scope, $http,$loction,$routeParams) {
+    console.log('PrescriptionsController loaded...');
+
+    $scope.getPrescriptions = function () {
+        $http.get('/api/prescriptions').then(successCallback,errorCallback);
+        function successCallback(response){
+            $scope.prescriptions=response.data;
+            console.log($scope.prescriptions)
+        }
+        function errorCallback(error) {
+            console.log("Error in prescription controller");
+        }
+    }
+}]);
