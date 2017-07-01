05caaf21ade71f064cbd0422954d60f191ce57ce
arg0nw <arunawickram@gmail.com>
2017-07-01 07:41:42 -0700

    auth dash

--- client/controllers/auth.js
+++ client/controllers/auth.js
@@ -2,23 +2,9 @@ var myApp = angular.module('myApp');
 myApp.controller('AuthController', ['$scope', '$http', '$location', '$routeParams', function ($scope, $http, $location, $routeParams) {
 	console.log('AuthController');
 
-	debugger;
-	var uname='';
-	var pwd ='';
-
-	uname = $scope.username;
-	pwd = $scope.password;
-
-<<<<<<< HEAD
-	/*$scope.signin = function () {
-		// if (username == "admin" && password == "admin") {
-			localStorage.setItem("username", username);
-=======
-	$scope.signin = function () {
+		$scope.signin=function(){
 		if (( $scope.username == "admin") && ($scope.password == "admin")) {
->>>>>>> 72ed68949f58b4424d9af7542144db3e8e16c862
 			$location.path('/dash');
-
 		}
 		else if (( $scope.username == "doc") && ($scope.password == "doc")) {
 			$location.path('/prescriptions');
@@ -30,18 +16,6 @@ myApp.controller('AuthController', ['$scope', '$http', '$location', '$routeParam
 		else {
 			window.alert("Error in login!");
 		}
-	}
-*/
+		}
 
-$scope.signin =function(usr,pwd){
-            console.log(usr+'/'+pwd);
-            if(usr=='admin' && pwd=='admin')
-            {
-                window.location.href='#!/dash';
-            }
-            else
-            {
-                window.location.href='#!/auth';
-            }
-        }
 }]);
