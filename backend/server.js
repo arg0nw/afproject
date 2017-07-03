87a52729ef6ebbea0d66feded0aff0c4da139c52
arg0nw <arunawickram@gmail.com>
2017-07-03 21:12:04 +0530

    finalize

--- backend/server.js
+++ backend/server.js
@@ -17,13 +17,13 @@ var Set = require('./models/sets.js');
 var IssuedPrescription = require('./models/issuedPrescriptions.js');
 
 //Connection
-// mongoose.connect("mongodb://localhost:27017/test", function (err, db) {
-//     if (!err) {
-//         console.log("we are connected to mongo");
-//     }
-// })
+mongoose.connect("mongodb://localhost:27017/test", function (err, db) {
+    if (!err) {
+        console.log("we are connected to mongo");
+    }
+})
 
-mongoose.connect('mongodb://danika:qwerty@ds139242.mlab.com:39242/pharmdeus')
+//mongoose.connect('mongodb://danika:qwerty@ds139242.mlab.com:39242/pharmdeus')
 //mongoose.connect('mongodb://samitha071:asd123pharmdeus-shard-00-00-qmi26.mongodb.net:27017,pharmdeus-shard-00-01-qmi26.mongodb.net:27017,pharmdeus-shard-00-02-qmi26.mongodb.net:27017/pharmdeus?ssl=true&replicaSet=pharmdeus-shard-0&authSource=admin');
 var db = mongoose.connection;
 
@@ -46,96 +46,6 @@ var transporter = nodemailer.createTransport({
 });
 
 console.log(__dirname);
-app.get('/api/geners', function(req,res){
-	Gener.getGeners(function(err,geners){
-		if(err)
-		{
-			throw err;
-		}
-		res.json(geners);
-	})
-});
-app.post('/api/geners', function(req,res){
-	var gener=req.body;
-	Gener.addGeners(gener, function(err,gener){
-		if(err)
-		{
-			throw err;
-		}
-		res.json(gener);
-	})
-});
-app.put('/api/geners/:_id', function(req,res){
-	var id=req.params._id;
-	var gener=req.body;
-	Gener.updateGeners(id,gener,{}, function(err,gener){
-		if(err)
-		{
-			throw err;
-		}
-		res.json(gener);
-	})
-});
-app.delete('/api/geners/:_id', function(req,res){
-	var id=req.params._id;
-	Gener.deleteGeners(id, function(err,gener){
-		if(err)
-		{
-			throw err;
-		}
-		res.json(gener);
-	})
-});
-app.get('/api/books', function(req,res){
-	Book.getBooks(function(err,books){
-		if(err)
-		{
-			throw err;
-		}
-		res.json(books);
-	})
-});
-app.get('/api/books/:_id', function(req,res){
-	Book.getBookByID(req.params._id,function(err,book){
-		if(err)
-		{
-			throw err;
-		}
-		res.json(book);
-	})
-});
-app.post('/api/books', function(req,res){
-	var book=req.body;
-	Book.addBooks(book, function(err,book){
-		if(err)
-		{
-			throw err;
-		}
-		res.json(book);
-	})
-});
-app.put('/api/books/:_id', function(req,res){
-	var id=req.params._id;
-	var book=req.body;
-	Book.updateBooks(id,book,{}, function(err,book){
-		if(err)
-		{
-			throw err;
-		}
-		res.json(book);
-	})
-});
-app.delete('/api/books/:_id', function(req,res){
-	var id=req.params._id;
-	Book.deleteBooks(id, function(err,book){
-		if(err)
-		{
-			throw err;
-		}
-		res.json(book);
-	})
-});
-
 app.get('/api/drugs',function(req,res){
     Drug.getDrugs(function(err,drugs)
     {
--- client/app.js
+++ client/app.js
@@ -6,10 +6,6 @@ myApp.config(function ($routeProvider) {
 			controller: 'DashController',
 			templateUrl: 'views/dash.html'
 		})
-		.when('/books', {
-			controller: 'BooksController',
-			templateUrl: 'views/books.html'
-		})
 		.when('/auth', {
 			controller: 'AuthController',
 			templateUrl: 'views/auth.html'
@@ -21,18 +17,6 @@ myApp.config(function ($routeProvider) {
 		.when('/newuser', {
 			controller: 'NewUController',
 			templateUrl: 'views/new_user.html'
-		})
-		.when('/books/details/:id', {
-			controller: 'BooksController',
-			templateUrl: 'views/books_details.html'
-		})
-		.when('/books/add', {
-			controller: 'BooksController',
-			templateUrl: 'views/add_book.html'
-		})
-		.when('/books/edit/:id', {
-			controller: 'BooksController',
-			templateUrl: 'views/edit_book.html'
 		})
 		.when('/drugs', {
 			controller: 'DrugsController',
