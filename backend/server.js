1480b73c7f3c6cba34d9b03bfd9ea78bf56cda5d
arg0nw <arunawickram@gmail.com>
2017-07-05 23:22:16 +0530

    Error Correction

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,6 @@ var nodemailer = require('nodemailer');
 app.use(bodyParser.json());
 app.use(express.static(__dirname+'/../client'));
 var Gener= require('./models/geners.js');
-var Book= require('./models/books.js');
 var Drug= require('./models/drugs.js');
 var Prescription= require('./models/prescriptions.js');
 var Email= require('./models/emails.js');
@@ -17,13 +16,13 @@ var Set = require('./models/sets.js');
 var IssuedPrescription = require('./models/issuedPrescriptions.js');
 
 //Connection
-mongoose.connect("mongodb://localhost:27017/test", function (err, db) {
-    if (!err) {
-        console.log("we are connected to mongo");
-    }
-})
+// mongoose.connect("mongodb://localhost:27017/test", function (err, db) {
+//     if (!err) {
+//         console.log("we are connected to mongo");
+//     }
+// })
 
-//mongoose.connect('mongodb://danika:qwerty@ds139242.mlab.com:39242/pharmdeus')
+mongoose.connect('mongodb://danika:qwerty@ds139242.mlab.com:39242/pharmdeus')
 //mongoose.connect('mongodb://samitha071:asd123pharmdeus-shard-00-00-qmi26.mongodb.net:27017,pharmdeus-shard-00-01-qmi26.mongodb.net:27017,pharmdeus-shard-00-02-qmi26.mongodb.net:27017/pharmdeus?ssl=true&replicaSet=pharmdeus-shard-0&authSource=admin');
 var db = mongoose.connection;
 
